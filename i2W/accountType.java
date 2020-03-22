f2465d932dbde3399ef0021408035b75d29ce589
EjaazLakhani1 <60117208+EjaazLakhani1@users.noreply.github.com>
2020-03-21 20:14:52 -0600

    Add files via upload

---
+++ i2W/accountType.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.util.HashMap;
+
+public class accountType {
+
+	static HashMap<String, Integer> acntType =  new HashMap<String, Integer>();
+		
+	public HashMap<String, Integer> getTypeMap() {
+			return acntType;
+	}
+
+}
+
