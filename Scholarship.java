c2bcfde95cb87815d50c41af9c582abb5ff6bdeb
Jan Jelinek <47360894+Jan-Jelinek@users.noreply.github.com>
2020-03-22 01:28:53 -0600

    Add files via upload

---
+++ Scholarship.java
@@ -0,0 +1,17 @@
+
+public class Scholarship {
+
+	private String name;			// Name
+	private String donor;			// Who gave out the scholarship
+	private int value;				// How much that scholarship gives
+	private String term;			// Fall/Winter/String/Summer/Full Year
+	private String type;			// Graduate/Undergraduate
+	private String department;		// i.e. computer science students only
+	private String[] applicants;	// List of student emails
+	private int deadline;			// YYYYMMDD
+	
+	public Scholarship() {
+				
+	}
+
+}
