4a95353c47cfae654107a11fe68b91747c27e81c
EjaazLakhani1 <60117208+EjaazLakhani1@users.noreply.github.com>
2020-03-23 10:26:47 -0600

    My final updated code for iteration 2

---
+++ i2Final/MainFrame.java
@@ -0,0 +1,37 @@
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.util.HashMap;
+
+public class MainFrame {
+
+	private JFrame frame;
+	private Authenticator auth = new Authenticator();
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrame window = new MainFrame();
+					window.frame.setVisible(true);
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	public MainFrame() {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int screenHeight = screenSize.height;
+		int screenWidth = screenSize.width;
+		frame = new JFrame();
+		frame.setBounds((screenWidth/2 - screenWidth/4), (screenHeight/2 - screenHeight/4), screenWidth/2, screenHeight/2);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Login panel = new Login(frame, auth);
+		frame.setContentPane(panel);
+		frame.revalidate();
+	}
+
+}
---
+++ i2Final/StudentMenu.java
@@ -0,0 +1,80 @@
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.Font;
+
+import java.awt.Color;
+import javax.swing.SwingConstants;
+
+public class StudentMenu extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+	
+	//NEED VARIABLES FOR NEW MENU
+
+	/**
+	 * Create the panel.
+	 * @param auth 
+	 * @param frame 
+	 */
+	public StudentMenu(JFrame frame, Authenticator auth) {
+		//Save the user's screen resolution to variables, used to format GUI correctly
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int screenHeight = screenSize.height;
+		int screenWidth = screenSize.width;
+
+		setLayout(null);
+
+		/**
+		 * LABELS
+		 */
+
+		//Header of the system name
+		JLabel header = new JLabel("UofC Student Scholarship Portal");
+		header.setHorizontalAlignment(SwingConstants.CENTER);
+		header.setForeground(Color.RED);
+		header.setBounds(180, 36, 526, 36);
+		header.setFont(new Font("Arial", Font.PLAIN, screenHeight/30));
+		add(header);
+
+		//Font size for remaining labels
+		Font labelFontSize = new Font("Arial", Font.PLAIN, screenHeight/60);
+			
+		/**
+		 * TEXT FIELDS
+		 */
+
+		//Text field for searching for a scholarship name
+		//Text field for inputing student's GPA (They will need to upload actual "transcript" for confirmation of this)
+
+		/**
+		 * MESSAGES
+		 */
+
+		//Create any error or confirmation messages here
+		//Error message if student doesn't meet requirements for a specific scholarship
+		
+		/**
+		 * BUTTONS
+		 */
+
+		//Create a button to upload a "transcript" to the system
+		//Create a button to confirm selection of transcript selected from the list to apply for
+		//Create a button to allow user to navigate to page of scholarships they have applied to and can check status
+
+		/**
+		 * LIST
+		 */
+
+		//Create a scrollable list showing all available scholarships, including details like for what term, faculty, and name of scholarship
+	}
+}
