d2529d8bccf5f87348cab99a6409f4a8e03bd96d
EjaazLakhani1 <60117208+EjaazLakhani1@users.noreply.github.com>
2020-03-23 11:56:55 -0600

    Final updated code for iteration 2

---
+++ iteration2Final/Authenticator.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class Authenticator {
+	
+	
+
+	static HashMap<String, String> accounts =  new HashMap<String, String>();
+	
+
+	public HashMap<String, String> getPeopleMap() {
+		return accounts;
+	}
+
+}
---
+++ iteration2Final/accountType.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.util.HashMap;
+
+public class accountType {
+
+	static HashMap<String, Integer> acntType =  new HashMap<String, Integer>();
+		
+	public HashMap<String, Integer> getTypeMap() {
+			return acntType;
+	}
+
+}
