0c7fce239f009e90f91f0e454a9434cd3a5c33f2
koladetyk <47436776+koladetyk@users.noreply.github.com>
2020-03-30 03:49:58 -0600

    Add files via upload

---
+++ MainFrame.java
@@ -0,0 +1,35 @@
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+public class MainFrame {
+
+	private JFrame frame;
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrame window = new MainFrame();
+					window.frame.setVisible(true);
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	public MainFrame() {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int screenHeight = screenSize.height;
+		int screenWidth = screenSize.width;
+		frame = new JFrame();
+		frame.setBounds((screenWidth/2 - screenWidth/4), (screenHeight/2 - screenHeight/4), screenWidth/2, screenHeight/2);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Login panel = new Login(frame);
+		frame.setContentPane(panel);
+		frame.revalidate();
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# SENG300Group22
+SENG 300 Winter 2020 Group 22
+
+## System Requirements
+Ensure that the user has a form of Java installed for use of the system application.
+
+## Installation and Usage
+1. Download the files from the branch DEMO-ITERATION2 as a zip file or a clone <br/>
+2. Compile the java files depending on how you downloaded the branch
+3. Run the MainFrame.java file
+
+**If you have Microsoft VSC downloaded, you can open the code and run the program inside the MainFrame.java file**
+
+## Authors
+McCurdy, Robert <br/>
+Lakhani, Ejaaz <br/>
+Jelinek, Jan <br/>
+Erinle, Ayokolade Emmanuel <br/>
+Gingrich, Richard
